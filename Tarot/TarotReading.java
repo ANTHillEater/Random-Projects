b04d5e3c7bf0b33330359601afdf29c9e0fb2e8e
Matthew Wasylyk <andrewdoesemail123@gmail.com>
2022-11-07 13:13:32 -0500

    more stuff

--- Tarot/TarotReading.java
+++ Tarot/TarotReading.java
@@ -1,6 +1,11 @@
 package Tarot;
 
 public class TarotReading {
+	private String [] cards = {"The Fool", "The Magician", "The High Preistess", "The Empress",
+	"The Emperor", "The Heiophant", "The Lovers", "The Chariot", "Justice",
+	"The Hermit", "Wheel of Fortune", "Strength", "The Hanged Man", "Death",
+	"Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
+	"Judgement", "The World"};
 	public static void main (String [] args){
 		//list of spread options
 	}
