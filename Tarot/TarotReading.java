252de050cb8be8fdacfeeaf62288d9f54a12d41e
Matthew Wasylyk <andrewdoesemail123@gmail.com>
2022-11-07 13:14:01 -0500

    minor

--- Tarot/TarotReading.java
+++ Tarot/TarotReading.java
@@ -1,11 +1,12 @@
 package Tarot;
 
 public class TarotReading {
-	private String [] cards = {"The Fool", "The Magician", "The High Preistess", "The Empress",
+	private String [] major = {"The Fool", "The Magician", "The High Preistess", "The Empress",
 	"The Emperor", "The Heiophant", "The Lovers", "The Chariot", "Justice",
 	"The Hermit", "Wheel of Fortune", "Strength", "The Hanged Man", "Death",
 	"Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
 	"Judgement", "The World"};
+	private String [] minor = {""};
 	public static void main (String [] args){
 		//list of spread options
 	}
