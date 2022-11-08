824e895d18552c19dc3b4abc4bf1f3fa0976ffe5
Matthew Wasylyk <andrewdoesemail123@gmail.com>
2022-11-08 10:20:23 -0500

    very nice

--- Tarot/TarotReading.java
+++ Tarot/TarotReading.java
@@ -1,13 +1,47 @@
 package Tarot;
 
+import java.util.*;
+
 public class TarotReading {
-	private String [] major = {"The Fool", "The Magician", "The High Preistess", "The Empress",
-	"The Emperor", "The Heiophant", "The Lovers", "The Chariot", "Justice",
-	"The Hermit", "Wheel of Fortune", "Strength", "The Hanged Man", "Death",
-	"Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
-	"Judgement", "The World"};
-	private String [] minor = {""};
 	public static void main (String [] args){
-		//list of spread options
+		String [] major = {"The Fool", "The Magician", "The High Preistess", "The Empress",
+					"The Emperor", "The Heiophant", "The Lovers", "The Chariot", "Justice",
+					"The Hermit", "Wheel of Fortune", "Strength", "The Hanged Man", "Death",
+					"Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
+					"Judgement", "The World"};
+		String [] minor = {""};
+		String [][] spreads = {{"Your card: "}, {"The Querant: ", "The Past: ", "The Present: ", "The Future: "},
+					{"There's a lot of these: "}};
+		int draw = 0;
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Choose a spread:");
+		System.out.println("1. Single card");
+		System.out.println("2. 4 card spread");
+		System.out.println("3. Celtic Cross");
+
+		switch(input.nextInt()){
+			case 1:
+				draw = (int)(Math.random() * 22);
+				System.out.println(spreads[0][0] + major[draw]);
+				break;
+			case 2:
+				for(int i = 0; i < 4; i++){
+					draw = (int)(Math.random() * 22);
+					System.out.println(spreads[1][i] + major[draw]);
+				}
+				break;
+			case 3:
+				for(int i = 0; i < 12; i++){
+					draw = (int)(Math.random() * 22);
+					System.out.println(spreads[2][0] + major[draw]);	//put "i" here when Celtic cross is finished
+				}
+				break;
+			default:
+				System.out.println("Thats not an option");
+				break;
+		}
+
+		input.close();
 	}
 }
